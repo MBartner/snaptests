586825597631db1ced0437f3610a4e1ac5d1bd84
Mike Bartner <m.bartner16@hotmail.com>
2018-06-25 00:52:40 -0400

    bart test updates

--- snapLenseProjects/bartner_test1/Public/Rotate.js
+++ snapLenseProjects/bartner_test1/Public/Rotate.js
@@ -1,22 +1,39 @@
 // -----JS CODE-----
-
+//print(Date());
 if (global.firstAccess === true){
 	global.firstAccess = false;
 	global.baseLocation = script.getSceneObject().getTransform().getLocalPosition();
+	global.baseRotation = script.getSceneObject().getTransform().getLocalRotation();
+	global.baseY = global.baseRotation.toEulerAngles().y;
 }
 
-var current = script.getSceneObject().getTransform().getLocalPosition();
+var currentLocation = script.getSceneObject().getTransform().getLocalPosition();
+var currentRotation = script.getSceneObject().getTransform().getLocalRotation();
 
-if (current.y > global.baseLocation.y + 10){
+if (currentLocation.y > global.baseLocation.y + 10){
 	global.up = false;
 }
-else if (current.y <= global.baseLocation.y){
+else if (currentLocation.y <= global.baseLocation.y){
 	global.up = true;
 }
 
-if (global.up){
-	script.getSceneObject().getTransform().setLocalPosition(new vec3(current.x, current.y + 1, current.z));
+/*if (global.up){
+	script.getSceneObject().getTransform().setLocalPosition(new vec3(currentLocation.x, currentLocation.y + 1, currentLocation.z));
 }
 else {
-	script.getSceneObject().getTransform().setLocalPosition(new vec3(current.x, current.y - 1, current.z));
-}
+	script.getSceneObject().getTransform().setLocalPosition(new vec3(currentLocation.x, currentLocation.y - 1, currentLocation.z));
+}*/
+
+var e = currentRotation.toEulerAngles();
+print("x=" + e.x + " y=" + e.y + " z=" + e.z);
+global.baseY = global.baseY + 0.1;
+var newE = new vec3(e.x /*+ 0.25*/, global.baseY /*e.y + 0.01*/, e.z /*+ 0.25*/);
+
+/*if(e.y > 1.55){
+	print("" + (e.y + 0.01));
+	var t = quat.fromEulerVec(newE).toEulerAngles();
+	print("New: x=" + t.x + " y=" + t.y + " z=" + t.z);
+}*/
+
+var newQ = quat.fromEulerVec(newE);
+script.getSceneObject().getTransform().setLocalRotation(newQ);
